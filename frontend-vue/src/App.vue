c60c330efa63606158765e7fb805a35264c9a30c
YimHyeyeong <87461599+YimHyeyeong@users.noreply.github.com>
2024-10-07 16:40:52 +0900

    test

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <div>수정완료</div>
+    <div>222222수정완료22222</div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
